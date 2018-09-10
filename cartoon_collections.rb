a0f9aabcf85b0c147f83778bd6bd6ac9d543ca71
jameslee1187@gmail.com <jameslee1187@gmail.com>
2018-09-10 07:05:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+end
 end
