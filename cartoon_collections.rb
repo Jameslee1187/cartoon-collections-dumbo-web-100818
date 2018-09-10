f5bab6b5ba1a8ba41d59ac50aa7e2d04c7a112b9
jameslee1187@gmail.com <jameslee1187@gmail.com>
2018-09-10 06:53:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index do |names, i|
+    puts "#{i + 1}. #{names}"
+  end
+end
+  
+  # code an argument here
   # Your code here
 end
 
